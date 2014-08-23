9486ecdc483cf5b6f42050f2411834c94fff7f7b
Ryan Andonian <ryanandonian@gmail.com>
2014-08-22 21:40:02 -0700

    Added player object.
    Player is roughly 4 units wide (40 meters)

---
+++ ConnectedWorlds/Assets/Scripts/GameObjects/PlayerController.cs.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		DrawDebug();
+	}
+
+	private void DrawDebug(){
+		Debug.DrawRay(transform.position, rigidbody2D.velocity, Color.green);
+		Debug.DrawRay(transform.position, transform.up * 15.0f, Color.white);
+		Debug.DrawRay(transform.position, transform.right * 15.0f, Color.yellow);
+	}
+}
