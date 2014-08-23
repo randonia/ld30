4d8fab3054ef0a5c48350defc355750717e65b67
Ryan Andonian <ryanandonian@gmail.com>
2014-08-23 16:47:19 -0700

    Adds LineRenderer to indicate via "tractor beam" guide the player where to stop their ship. Next step: Docking logic

---
+++ ConnectedWorlds/Assets/Scripts/GameObjects/SortingController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Reflection;
+
+
+public class SortingController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		renderer.sortingLayerName = "fx_mid";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private string[] GetSortingLayerNames() {
+		var internalEditorUtilityType = typeof(InternalEditorUtility);
+		PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+		return (string[])sortingLayersProperty.GetValue(null, new object[0]);
+	}
+
+}
--- ConnectedWorlds/Assets/Scripts/GameObjects/StationController.cs
+++ ConnectedWorlds/Assets/Scripts/GameObjects/StationController.cs
@@ -1,19 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using ConnectedWorldsEngine;
 
 public class StationController : CWMonoBehaviour {
 
+	#region PREFABS
+	public Object GO_LRPREFAB;
+	#endregion
+
+	public Vector2 mBeamOffset;
+
+	private List<GameObject> mNearbyShips;
+	private List<GameObject> mLineRenderers;
+	
 	// Use this for initialization
 	void Start () {
-	
+		mNearbyShips = new List<GameObject>();
+		mLineRenderers = new List<GameObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		for(int i = 0; i < mNearbyShips.Count; ++i){
+			mLineRenderers[i].GetComponent<LineRenderer>().SetPosition(0, ((Vector2)gameObject.transform.position + mBeamOffset));
+			mLineRenderers[i].GetComponent<LineRenderer>().SetPosition(1, mNearbyShips[i].transform.position);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		Debug.Log(other.gameObject.name + " entered trigger zone");
+		if (other.GetComponent<CWMonoBehaviour>().checkFlags(ObjectFlags.SHIP))
+		{
+			addShip(other.gameObject);
+		}
 	}
+
+	void OnTriggerExit2D(Collider2D other){
+		Debug.Log (other.gameObject.name + " leaving trigger zone");
+		removeShip(other.gameObject);
+	}
+
+	void addShip(GameObject other){
+		mNearbyShips.Add(other);
+		mLineRenderers.Add((GameObject)GameObject.Instantiate(GO_LRPREFAB, transform.position, transform.rotation));
+	}
+
+	void removeShip(GameObject other){
+		int shipIndex = mNearbyShips.IndexOf(other);
+		mNearbyShips.RemoveAt(shipIndex);
+		GameObject objToDestroy = mLineRenderers[shipIndex];
+		mLineRenderers.RemoveAt(shipIndex);
+		GameObject.Destroy(objToDestroy);
+	}
+
 }
