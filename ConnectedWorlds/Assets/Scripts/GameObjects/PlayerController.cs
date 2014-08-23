4c89c43e236ff11c51cd39a4f0411ccd268534f2
Ryan Andonian <ryanandonian@gmail.com>
2014-08-23 02:06:33 -0700

    Adds SAS system! R button for now.
    Also disables stabilization if you're moving.

--- ConnectedWorlds/Assets/Scripts/GameObjects/PlayerController.cs
+++ ConnectedWorlds/Assets/Scripts/GameObjects/PlayerController.cs
@@ -7,10 +7,23 @@ public class PlayerController : MonoBehaviour {
 
 	// The main camera
 	public GameObject GO_mainCamera;
+	public GameObject GO_sasInside;
+	public GameObject GO_sasLabel;
+
+	public GameObject GO_DEBUG;
 
 	#endregion
 
 	#region Member variables
+
+	private UILabel DEBUGLABEL;
+
+	private bool mSAS_ENABLED = false;
+	private bool mTempSAS_DISABLE = false;
+
+	private Color kDisableColor = Color.Lerp(Color.green, Color.black, 0.8f);
+	private Color kTempDisableColor = Color.Lerp(Color.green, Color.grey, 0.5f);
+	private Color kEnableColor = Color.green;
 
 	#endregion
 
@@ -22,11 +35,21 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		DEBUGLABEL = GO_DEBUG.GetComponent<UILabel>();
+		DEBUGLABEL.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Controls
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			mSAS_ENABLED = !mSAS_ENABLED;
+		}
+		mTempSAS_DISABLE = (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E));
+
+		toggleSASButton();
 
 		// Translation
 		Vector2 thrustVec = new Vector2(Input.GetAxis("thrust"), Input.GetAxis ("strafe"));
@@ -35,9 +58,13 @@ public class PlayerController : MonoBehaviour {
 		rigidbody2D.AddForce(transform.up * thrustVec.y * thrustScale);
 
 		// Rotation
-		float rotScale = 0.25f;
-		float rotAmount = Input.GetAxis("rotate");
-		rigidbody2D.AddTorque(rotAmount * rotScale);
+		if(!mSAS_ENABLED || mTempSAS_DISABLE){
+			float rotScale = 0.35f;
+			float rotAmount = Input.GetAxis("rotate");
+			rigidbody2D.AddTorque(rotAmount * rotScale);
+		} else {
+				rigidbody2D.AddTorque(-(rigidbody2D.angularVelocity * 0.05f));
+		}
 
 		// Update the camera
 		GO_mainCamera.transform.position = Vector3.zero;
@@ -50,5 +77,10 @@ public class PlayerController : MonoBehaviour {
 		Debug.DrawRay(transform.position, rigidbody2D.velocity, Color.green);
 		Debug.DrawRay(transform.position, transform.up * 4.0f, Color.white);
 		Debug.DrawRay(transform.position, transform.right * 4.0f, Color.yellow);
+	}
+
+	private void toggleSASButton(){
+		GO_sasInside.GetComponent<UISprite>().color = (!mTempSAS_DISABLE && mSAS_ENABLED)?kEnableColor:(mTempSAS_DISABLE && mSAS_ENABLED)?kTempDisableColor:kDisableColor;
+		GO_sasLabel.GetComponent<UILabel>().color = (!mTempSAS_DISABLE && mSAS_ENABLED)?Color.white:kDisableColor;
 	}
 }
