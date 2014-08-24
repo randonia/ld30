e7577ce01803f1f5d0b5466a5468062b79879b0c
Ryan Andonian <ryanandonian@gmail.com>
2014-08-24 09:08:51 -0700

    Fixes the Bullet collision

--- ConnectedWorlds/Assets/Scripts/Weapons/Bullet.cs
+++ ConnectedWorlds/Assets/Scripts/Weapons/Bullet.cs
@@ -14,10 +14,14 @@ public class Bullet : Projectile {
 		CWMonoBehaviour cwmb = other.GetComponent<CWMonoBehaviour>();
 		if(cwmb != null){
 			Debug.Log("I hit " + other.gameObject.name);
-			if(cwmb.checkFlags(ObjectFlags.PLAYER)){
-				cwmb.GetComponent<PlayerController>().takeDamage(mDamage);
-				Debug.DrawRay(transform.position - transform.right, transform.right * 2, Color.red, 2.0f);
-				Debug.DrawRay(transform.position - transform.up, transform.up * 2, Color.red, 2.0f);
+			if(cwmb.checkFlags(ObjectFlags.SHIP)){
+				if(!cwmb.checkFlags(mObjectFlags))
+				{
+					Debug.Log("Inside of the second check");
+					cwmb.GetComponent<CWMonoBehaviour>().takeDamage(mDamage);
+					Debug.DrawRay(transform.position - transform.right, transform.right * 2, Color.red, 2.0f);
+					Debug.DrawRay(transform.position - transform.up, transform.up * 2, Color.red, 2.0f);
+				}
 			}
 		}
 	}
