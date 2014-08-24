517fb6e4fd9ec0ee4959a84171cc6e748dd4d388
Ryan Andonian <ryanandonian@gmail.com>
2014-08-24 01:48:34 -0700

    Adds some life to these asteroids

--- ConnectedWorlds/Assets/AsteroidController.cs
+++ ConnectedWorlds/Assets/AsteroidController.cs
@@ -10,8 +10,14 @@ public class AsteroidController : CWMonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float randomTorque = Random.Range(-100.0f, 100.0f);
-		rigidbody2D.AddTorque(randomTorque);
+		float randScale = Random.Range(0.8f, 1.4f);
+		float randomTorque = Random.Range(10.0f, 100.0f);
+
+		transform.rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+		transform.localScale = transform.localScale * randScale;
+		rigidbody2D.mass = rigidbody2D.mass * randScale;
+		bool ccwOrCw = Random.value > 0.5f;
+		rigidbody2D.AddTorque(randomTorque * ((ccwOrCw)?-1:1));
 	}
 	
 	// Update is called once per frame
